950cb5d524d45fbc32cabf73b43eee3bac3f7ab7
kiy0taka <kiy0taka333+github@gmail.com>
2011-10-22 23:43:55 +0900

    Typo.url should be valid url.

--- grails-app/domain/translate/Typo.groovy
+++ grails-app/domain/translate/Typo.groovy
@@ -18,7 +18,7 @@ class Typo {
     Date lastUpdated
 
     static constraints = {
-        url nullable:false,blank:false
+        url nullable:false,blank:false,url:true
         content nullable:false,blank:false,maxSize:999999
         comment nullable:false,blank:false,comment:999999
         dateCreated display:false
